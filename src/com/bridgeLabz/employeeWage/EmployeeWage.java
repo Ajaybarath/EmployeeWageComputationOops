7a778ef73334e7f3fb3f4bf4bc88992c51e9b3b1
ajaybarath <ajaybarath3@gmail.com>
2021-08-25 19:12:08 +0530

    ADDED: part time check and wage calculation

--- src/com/bridgeLabz/employeeWage/EmployeeWage.java
+++ src/com/bridgeLabz/employeeWage/EmployeeWage.java
@@ -4,6 +4,7 @@ public class EmployeeWage {
 	
 	private static final int EmployeeWagePerHour = 20;
 	private static final int FullDayHours = 8;
+	private static final int PartTimeHours = 4;
 	
 	public static void main(String args[]) {
 
@@ -29,7 +30,12 @@ public class EmployeeWage {
 		int employeeWage = 0;
 		
 		if (checkAttendence()) {
-			employeeWage += EmployeeWagePerHour * FullDayHours;
+			if (partTimeOrFullTimeCheck()) {
+				employeeWage += EmployeeWagePerHour * PartTimeHours;
+			}
+			else {
+				employeeWage += EmployeeWagePerHour * FullDayHours;	
+			}
 			
 			System.out.println("Employee is present and his wage is " + employeeWage);
 		}
@@ -37,6 +43,17 @@ public class EmployeeWage {
 			System.out.println("Employee is absent");
 		}
 
+	}
+	
+	public boolean partTimeOrFullTimeCheck() {
+		
+		int employeeWorkStatus = (int) (Math.floor(Math.random() * 10) % 2); // 1 means fulltime, 0 means partime
+
+		if (employeeWorkStatus == 0) {
+			return false;
+		}
+		
+		return true;
 	}
 
 }
